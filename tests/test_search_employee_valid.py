d2b313c4ad6b8929ef75f0c0caec1eb3cf4b7474
Diana Akhmatova <1032216245@pfur.ru>
2025-05-31 19:30:34 -0700

    Delete tests/test_search_employee_valid.py

--- tests/test_search_employee_valid.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from pages.login_page import LoginPage
-
-def test_search_employee_valid(driver):
-    # TODO: implement test for Поиск сотрудника по имени
-    assert True  # placeholder
